48970420daeeaa130e3004e61c65a6db67a5e76b
oct1H <167855724+oct1H@users.noreply.github.com>
2024-05-29 12:00:13 +0900

    Delete post_1.js

--- post_1.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector("#menu_box>p");
-let toMenu = document.querySelector("#menu_box>div");
-
-menu.addEventListener("click",()=>{
-    toMenu.classList.toggle("open");
-});
