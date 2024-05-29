e8ab7059dc44f5c34fd2190abcc2f42e919d1673
oct1H <167855724+oct1H@users.noreply.github.com>
2024-05-29 11:40:51 +0900

    Add files via upload

--- java_script.js
+++ java_script.js
@@ -2,7 +2,5 @@ let menu = document.querySelector("#menu_box>p");
 let toMenu = document.querySelector("#menu_box>div");
 
 menu.addEventListener("click",()=>{
-    toMenu.classList.toggle("menu_close");
-    toMenu.classList.toggle("menu_open");
-    toMenu.classList.remove("none");
+    toMenu.classList.toggle("open");
 });
